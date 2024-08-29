eec796de011e61c141ad76ac5377eba8178ed626
Dwiky-fn <zencraft468@gmail.com>
2024-08-29 22:12:47 +0700

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -7,10 +7,7 @@ from datetime import datetime
 import locale
 
 # Atur locale ke bahasa Indonesia
-try:
-    locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
-except locale.Error:
-    locale.setlocale(locale.LC_ALL, 'C')  # Fallback ke locale default
+locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
 
 # Fungsi untuk mengatur font dan ukuran teks
 def set_font(run, font_name='Times New Roman', font_size=12):
